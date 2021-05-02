911839be8221bdeaf85712f32be1bbf3524e52b5
Hera <exefile69@gmail.com>
2021-05-02 18:46:03 +0400

    dice resources added

---
+++ app/src/main/java/com/hera/fate/utils/ImageCollection.kt
@@ -0,0 +1,12 @@
+package com.hera.fate.utils
+
+import com.hera.fate.R
+
+object ImageCollection {
+
+    val diceImages = listOf(
+        R.drawable.dice_1, R.drawable.dice_2,
+        R.drawable.dice_3, R.drawable.dice_4,
+        R.drawable.dice_5, R.drawable.dice_6
+    )
+}
